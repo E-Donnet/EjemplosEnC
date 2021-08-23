a8e121a015eac22afb7e4c158748f15ef73ca013
E-Donnet <2193079311@correo.ler.uam.mx>
2021-08-23 04:17:59 -0500

    EjemplosEnC "Hola UAM" Martes 17

---
+++ HOLAMUNDO.c
@@ -0,0 +1,41 @@
+/* HOLA MUNDO OH OH OH 
+
+
+Los comentarios son para nosotres <3 y para el mundo. 
+Se breve, se entendible 
+
+NOmbre 
+Autor 
+¡Qué es lo que hace un programa...
+
+Preguntas imaginarias
+Se necesitas un circuito 
+si es el caso, colocar la imagen del repositorio : liga
+terminales y modo (Entrada, Salida, Bidireccional)
+*/ 
+/*Bibliotecas
+Hay codigos ya hechos. 
+*/ 
+#include <stdio.h> 
+
+/*Declaraciones de variables*/ 
+/*Declarar las funciones*/ 
+
+
+
+/*Estructura de programas*/ 
+
+int  main()
+
+{
+
+            printf("Hola UAM");
+            return 0; 
+
+
+}
+
+
+
+
+
